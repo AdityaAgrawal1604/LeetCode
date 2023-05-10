a2414c94c9cb6f25b16c01db50533d0120057777
Aditya Agrawal <82768898+AdityaAgrawal1604@users.noreply.github.com>
2023-05-11 04:20:34 +0530

    Time: 244 ms (61.90%), Space: 114.1 MB (92.71%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *last = head;
+        if(!head->next){
+            return true;
+        }
+        while(last->next){
+            last = last->next;
+        }
+        ListNode *f = head;
+        ListNode *s = head;
+        f = head->next;
+        s = head;
+        while(f){
+            f = f->next;
+            if(f){
+                f = f->next;
+                s= s->next;
+            }
+        }
+        cout<<s->val<<" "<<s->next->val<<endl;
+        ListNode *p = s;
+        ListNode *q = s->next;
+        ListNode *r;
+        if(q->next){
+            r = q->next;
+        }
+        p->next = NULL;
+        while(r){
+            q->next = p;
+            p = q;
+            q = r;
+            r = r->next;
+        }
+        q->next = p;
+        ListNode *c1 = head;
+        ListNode *c2 = last;
+        cout<<c1->val<<" "<<c2->val<<endl;
+        while(c1&&c2){
+            if(c1->val!=c2->val){
+                return false;
+            }
+            c1 = c1->next;
+            c2 = c2->next;
+        }
+        return true;
+    }
+};
