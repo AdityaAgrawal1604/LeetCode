fe45ddcd25eaab7219bd568514d8e398ec83fd93
Aditya Agrawal <82768898+AdityaAgrawal1604@users.noreply.github.com>
2023-05-11 03:29:04 +0530

    Time: 54 ms (40.74%), Space: 14.5 MB (90.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,53 +9,24 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *p = headA;
-        ListNode *q = headB;
-        int cnt1 = 1;
-        while(p->next){
-            p = p->next;
-            cnt1++;
+        
+        if(headA==headB){
+            return headA;
         }
-        int cnt2 = 1;
-        while(q->next){
-            cnt2++;
-            q = q->next;
+        ListNode *a = headA;
+        ListNode *b = headB;
+        while(a!=b){
+            if(a==NULL){
+                a = headB;
+            }else{
+                a = a->next;
+            }
+            if(b==NULL){
+                b = headA;
+            }else{
+                b = b->next;
+            }
         }
-        // cout<<cnt1<<" "<<cnt2<<endl;
-        if(cnt1==cnt2){
-            p = headA;
-            q = headB;
-            if(p==q){
-                return p;
-            }
-            while(p->next&&q->next&&p->next!=q->next){
-                p = p->next;
-                q = q->next;
-            }
-            return p->next;
-        }else{
-            p = headA;
-            q = headB;
-             if(cnt1>cnt2){
-                 while(cnt1!=cnt2){
-                     cnt1--;
-                     p = p->next;
-                 }
-             }else{
-                 while(cnt1!=cnt2){
-                     cnt2--;
-                     q = q->next;
-                 }
-             }
-            // cout<<p->val<<" "<<q->val<<endl;
-            if(p==q){
-                return p;
-            }
-            while(p->next&&q->next&&p->next!=q->next){
-                p = p->next;
-                q = q->next;
-            }
-            return p->next;
-        }
+        return a;
     }
 };
