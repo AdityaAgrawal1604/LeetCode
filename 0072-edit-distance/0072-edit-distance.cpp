5c7b14ffba20a69662bf44c1dee4a1e209661876
Aditya Agrawal <82768898+AdityaAgrawal1604@users.noreply.github.com>
2023-05-13 10:28:06 +0530

    Time: 73 ms (15.85%), Space: 80.6 MB (14.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int f(string &s1,string &s2,int i,int j,vector<vector<int>> &dp){
-        if(i<0){
+    int f(int i,int j,string s1,string s2,vector<vector<int>> &dp){
+        if(i<0&&j<0){
+            return 0;
+        }
+        if(i<0&&j>=0){
             return j+1;
         }
-        if(j<0){
+        if(i>=0&&j<0){
             return i+1;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         if(s1[i]==s2[j]){
-            return dp[i][j]=f(s1,s2,i-1,j-1,dp);
+            return dp[i][j] = f(i-1,j-1,s1,s2,dp);
         }
-        int r = f(s1,s2,i-1,j-1,dp);
-        int in = f(s1,s2,i,j-1,dp);
-        int del = f(s1,s2,i-1,j,dp);
-        return dp[i][j] = (1+min(r,min(in,del)));
+        int op1 = 1+f(i,j-1,s1,s2,dp);
+        int op2 = 1+f(i-1,j,s1,s2,dp);
+        int op3 = 1+f(i-1,j-1,s1,s2,dp);
+        return dp[i][j] = min(op1,min(op2,op3));
     }
-    
     int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
+        int n = word1.size();
+        int m = word2.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(word1,word2,n-1,m-1,dp);
+        return f(n-1,m-1,word1,word2,dp);
     }
 };
