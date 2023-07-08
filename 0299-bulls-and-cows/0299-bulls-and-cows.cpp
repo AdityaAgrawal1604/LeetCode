3387635170890c396b512b573d1ccd784d805642
Aditya Agrawal <82768898+AdityaAgrawal1604@users.noreply.github.com>
2023-07-09 01:01:00 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (93.37%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -9,27 +9,25 @@ public:
         reverse(s.begin(),s.end());
         return s;
     }
-    string getHint(string secret, string guess) {
+    string getHint(string s, string g) {
         int bull = 0,cows = 0;
         string ans = "";
-        map<int,int> cnt1,cnt2;
-        
-        for(int i=0;i<secret.size();i++){
-            if(secret[i] == guess[i]){
+        vector<int> v(10,0);
+        for(int i = 0;i<g.size();i++){
+            if(s[i]!=g[i]){
+                v[g[i]-'0']++;
+            }
+        }
+        for(int i = 0;i<s.size();i++){
+            if(s[i]==g[i]){
                 bull++;
-            }
-            else{
-                cnt1[secret[i]]++;
-                cnt2[guess[i]]++;
-            }
-        }
-        
-        for(auto i:cnt1){
-            if(cnt2[i.first]){
-               cows += min(i.second,cnt2[i.first]);
+            }else{
+                if(v[s[i]-'0']){
+                    v[s[i]-'0']--;
+                    cows++;
+                }
             }
         }
-        
         cout<<bull<<" "<<cows<<endl;
         ans = (tost(bull));
         if(!bull) ans+="0";
