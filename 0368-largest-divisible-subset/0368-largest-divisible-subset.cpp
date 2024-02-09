038bec299c444febe6c196130f6c06e32724b0b5
Aditya Agrawal <82768898+AdityaAgrawal1604@users.noreply.github.com>
2024-02-10 01:27:01 +0530

    Time: 78 ms (7.84%), Space: 12.9 MB (6.61%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -16,39 +16,12 @@ public:
             int f = 0;
             for(int j=1;(j*j)<=nums[i];j++){
                 if(((nums[i]%j==0)&&(mp[j])&&((nums[i]/j)!=1))){
-                    // f = 1;
                     pos[i]=max(pos[i],pos[mp[j]-1]+1);
-                    // break;
                 }
                 if((nums[i]%j==0)&&(mp[nums[i]/j])&&((j)!=1)){
-                    // f = 1;
                     pos[i]=max(pos[i],pos[mp[nums[i]/j]-1]+1);
-                    // break;
                 }
             }
-            // if(f) continue;
-            // cout<<"x "<<x<<endl;
-            // int temp = 1;
-            // for(int j = (i+1);j<n;j++){
-            //     if((nums[j]%x)==0){
-            //         temp++;
-            //         x = nums[j];
-            //         cout<<x<<endl;
-            //     }
-            // }
-            // if(temp>maxi){
-            //     maxi = temp;
-            //     x = nums[i];
-            //     ans.clear();
-            //     ans.push_back(nums[i]);
-            //     for(int j = (i+1);j<n;j++){
-            //         if((nums[j]%x)==0){
-            //             x = nums[j];
-            //             ans.push_back(x);
-            //         }
-            //     }
-            // }
-            // cout<<temp<<endl;
         }
         int x = -1;
         for(int i = 0;i<n;i++){
@@ -56,11 +29,8 @@ public:
                 maxi = pos[i];
                 x = nums[i];
             }
-            // cout<<pos[i]<<" ";
         }
-        // cout<<endl;
         for(int i = (n-1);i>=0;i--){
-            // cout<<i<<" "<<maxi<<" "<<x<<" "<<nums[i]<<endl;
             if(((pos[i]==maxi))){
                 if(x%nums[i]) continue;
                 x = nums[i];
@@ -68,7 +38,6 @@ public:
                 maxi--;
             }
         }
-        // cout<<maxi<<endl;
         return ans;
     }
 };
